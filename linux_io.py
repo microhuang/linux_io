4ff19a9b29e56566ab7cb001c8ed1515bb04c89f
microhuang <microhuang@live.com>
2014-01-24 11:30:04 +0800

    Create linux_io.py

---
+++ linux_io.py
@@ -0,0 +1,101 @@
+//(同步)阻塞模型
+//请求/响应模式，阻塞式套接字如果需要实现全双工通迅，意味着程序为每个连接必须存在两个线程。
+
+//server
+'''
+socket
+  |
+bind
+  |
+listen
+  |
+accept
+  |
+read/write
+  |
+close
+'''
+
+//client
+'''
+socket
+  |
+connect                  
+  |
+write/read
+  |
+close
+'''
+
+
+//（同步）非阻塞模型
+//通过轮询，如果没有可读取数据内核直接返回未准备就绪的信号，等待用户程序的下一次轮询。
+
+//server
+'''
+socket
+  |
+bind
+  |
+listen
+  |
+accept
+  |
+read(null) -> read(null) -> read(null) -> ... read(data)
+  |
+write(null) -> write(null) -> write(null) -> ... write(data)
+  |
+close
+'''
+
+//client
+'''
+socket
+  |
+connect
+  |
+read(null) -> read(null) -> read(null) -> ... read(data)
+  |
+write(null) -> write(null) -> write(null) -> ... write(data)
+  |
+close
+'''
+
+
+//I/O复用模型
+//select、poll、epoll
+
+//server
+'''
+socket
+  |
+bind
+  |
+listen
+  |
+select  <=============
+  |                  |
+accept/read/write ====
+  |
+close
+'''
+
+//client
+'''
+socket
+  |
+connect
+  |
+select  <=============
+  |                  |
+read/write ===========
+  |
+close
+'''
+
+
+//信号驱动I/O模型
+
+
+
+//异步I/O模型
